c9dc6664be7c96b724d4571b77ae1be8fe57dc0c
vialocti <vabrizuela2011@gmail.com>
2022-07-31 15:52:19 -0300

    funcion suma adiciondo

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 let a = 1
+const suma = (a,b)=>{
+    return a + b;
+}
 const producto = (a,b)=>{
     return a * b;
 }
-
+let sumaT = suma(10,15);
 let pr = producto(12,45); 
 let nombre=prompt("ingrese su nombre");
 alert(nombre);
