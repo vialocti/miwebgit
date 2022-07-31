637f14fe9306ebbf23efb5a339ee48e3d142f1ad
vialocti <vabrizuela2011@gmail.com>
2022-07-31 15:49:33 -0300

    last commit master

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const producto = (a,b)=>{
     return a * b;
 }
 
-let pr = producto(12,45); 
+let pr = producto(12,45);
 let nombre=prompt("ingrese su nombre");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
